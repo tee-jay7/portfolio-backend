4b83e64be13e23c571a70a8d6d8b141f39465d5f
tee-jay7 <twasam.john@gmail.com>
2024-07-19 16:25:56 +0000

    Added code and tests

---
+++ app/function.py
@@ -0,0 +1,63 @@
+import json
+import boto3
+import os
+from datetime import datetime
+
+client = boto3.client('dynamodb')
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return json.JSONEncoder.default(self, obj)
+
+
+def lambda_handler(event, context):
+    response = client.describe_table(
+        TableName=os.getenv('TABLE_NAME')
+    )
+    return {
+        "statusCode": 200,
+        "headers": {
+             "Access-Control-Allow-Origin": '*',
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Hello World",
+            'table': response
+        }, cls=DateTimeEncoder)
+    }
+
+
+def visit_handler(event, context):
+    table_name = os.getenv('TABLE_NAME')
+    key_value = "page_counter"
+
+    response = client.update_item(
+        TableName=table_name,
+        Key={
+            'ID': {'S': key_value}
+        },
+        UpdateExpression="SET #attrName = if_not_exists(#attrName, :start) + :inc",
+        ExpressionAttributeNames={
+            "#attrName": "count" 
+        },
+        ExpressionAttributeValues={
+            ":start": {"N": "0"},
+            ":inc": {"N": "1"}
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": '*',
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Update successful",
+            "updated_value": response['Attributes']['count']['N']
+        })
+    }
+
